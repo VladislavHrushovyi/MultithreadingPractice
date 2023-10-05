0bb801593801e49ecc0c596df11725a1f8b401c5
vladg <vlad.grush.99@gmail.com>
2023-10-05 13:50:30 +0300

    use mutex for sync access to resource

---
+++ MultithreadingPractice/DoSomeProcess/WithMutex.cs
@@ -0,0 +1,26 @@
+﻿using MultithreadingPractice.Utils;
+
+namespace MultithreadingPractice.DoSomeProcess;
+
+public class WithMutex : ISomeProcess
+{
+    private readonly Mutex _mutex = new();
+    private readonly WriteToFile _writer = new();
+    public void WriteToFile(string text, string threadId)
+    {
+        _mutex.WaitOne();
+        try
+        {
+            _writer.WriteLineToFile("./test.txt", text, threadId);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            _mutex.ReleaseMutex();
+        }
+    }
+}
--- MultithreadingPractice/Program.cs
+++ MultithreadingPractice/Program.cs
@@ -1,13 +1,14 @@
 ﻿
 using MultithreadingPractice.DoSomeProcess;
 
-ISomeProcess withLock = new WithMonitor();
-Parallel.For(1, 100, (i) =>
+File.Delete("./test.txt");
+ISomeProcess withLock = new WithMutex();
+Parallel.For(1, 1000, (i) =>
 {
-    var threadId = Thread.CurrentThread.ManagedThreadId.ToString();
+    var threadId = Environment.CurrentManagedThreadId.ToString();
     withLock.WriteToFile($"{i}", threadId);
     Console.WriteLine($"{i} is done");
 });
 
-Console.WriteLine("Process with monitor completed");
+Console.WriteLine("Process completed");
 Console.ReadKey();
