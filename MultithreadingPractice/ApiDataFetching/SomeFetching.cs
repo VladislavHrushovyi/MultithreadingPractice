ed455d5ea24eeccdfb629cf3ecacd429fad259f1
vladg <vlad.grush.99@gmail.com>
2023-10-07 11:29:53 +0300

    non blocked main thread with fetching

---
+++ MultithreadingPractice/ApiDataFetching/SomeFetching.cs
@@ -0,0 +1,13 @@
+﻿namespace MultithreadingPractice.ApiDataFetching;
+
+public class SomeFetching
+{
+    private readonly Random _random = new();
+
+    public async Task FetchFromUrl(string url)
+    {
+        await Task.Delay(_random.Next(1000, 10000)); // some fetching
+
+        Console.WriteLine("Data from url: " + url);
+    }
+}
--- MultithreadingPractice/Program.cs
+++ MultithreadingPractice/Program.cs
@@ -1,14 +1,26 @@
 ﻿
+using MultithreadingPractice.ApiDataFetching;
 using MultithreadingPractice.DoSomeProcess;
 
-File.Delete("./test.txt");
-ISomeProcess withLock = new WithSemaphore();
-Parallel.For(1, 1000, (i) =>
-{
-    var threadId = Environment.CurrentManagedThreadId.ToString();
-    withLock.WriteToFile($"{i}", threadId);
-    Console.WriteLine($"{i} is done");
-});
+// File.Delete("./test.txt");
+// ISomeProcess withLock = new WithSemaphore();
+// Parallel.For(1, 1000, (i) =>
+// {
+//     var threadId = Environment.CurrentManagedThreadId.ToString();
+//     withLock.WriteToFile($"{i}", threadId);
+//     Console.WriteLine($"{i} is done");
+// });
+//
+// Console.WriteLine("Process completed");
 
-Console.WriteLine("Process completed");
+var fetchClient = new SomeFetching();
+var fetchingTask = Enumerable.Range(1, 100)
+    .Select(i => fetchClient.FetchFromUrl($"URL: {i}"))
+    .ToArray();
+while (!Task.WhenAll(fetchingTask).IsCompleted)
+{
+    Console.WriteLine("Awaiting fetching...");
+    await Task.Delay(200);
+}
+Console.WriteLine("FETCHED ALL DATA");
 Console.ReadKey();
